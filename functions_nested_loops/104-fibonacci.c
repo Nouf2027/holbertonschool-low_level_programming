f2d41615f9d0138e6315488c528cc8190d3518ab
root <root@ip-10-42-95-115.ec2.internal>
2025-10-11 15:20:32 +0000

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,39 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-* main - prints the first 98 Fibonacci numbers
-* Return: Always 0
+*main-prints the first 98 Fibonacci numbers,starting with 1 and 2
+*Return:Always 0
 */
 int main(void)
 {
-unsigned long a = 1, b = 2, next;
+unsigned long a=1,b=2,a_high,a_low,b_high,b_low,carry;
 int i;
-printf("%lu, %lu", a, b);
-for (i = 2; i < 98; i++)
+printf("%lu, %lu",a,b);
+for(i=2;i<98;i++)
 {
-next = a + b;
-printf(", %lu", next);
-a = b;
-b = next;
+if(a+b<10000000000)
+{
+unsigned long next=a+b;
+printf(", %lu",next);
+a=b;
+b=next;
+}
+else
+{
+if(i==2)
+{
+a_high=a/1000000000;
+a_low=a%1000000000;
+b_high=b/1000000000;
+b_low=b%1000000000;
+}
+carry=(a_low+b_low)/1000000000;
+b_low=(a_low+b_low)%1000000000;
+b_high=a_high+b_high+carry;
+printf(", %lu%09lu",b_high,b_low);
+a_high=b_high-a_high-carry+(a_low>b_low);
+a_low=b_low;
+}
 }
 printf("\n");
-return (0);
+return(0);
 }
