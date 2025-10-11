774c52292e8ede0a4d9acb3eafb353c9c3a89c61
root <root@ip-10-42-95-115.ec2.internal>
2025-10-11 12:08:20 +0000

    fix a compile error

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line
  */
+
 void print_alphabet(void)
 {
 int letter;
@@ -13,3 +14,4 @@ _putchar(letter);
 }
 _putchar('\n');
 }
+
