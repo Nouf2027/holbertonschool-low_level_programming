4ea3f16aef8b9c8b2ef22ea7b3eeba1fa47c1e0b
root <root@ip-10-42-95-115.ec2.internal>
2025-10-11 13:21:39 +0000

    Add files

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void times_table(void)
+{
+int i, j, n;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+            n = i * j;
+if (j == 0)
+_putchar('0');
+else
+{
+_putchar(',');
+_putchar(' ');
+if (n < 10)
+_putchar(' ');
+else
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
