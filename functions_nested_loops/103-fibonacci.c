a4c1e7924cc9f98d96cd226675b7d259e30bbaa7
root <root@ip-10-42-95-115.ec2.internal>
2025-10-11 15:06:56 +0000

    103-fibonacci.c

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - finds and prints the sum of the even-valued terms in Fibonacci sequence
- * whose values do not exceed 4,000,000
- * Return: Always 0
- */
+* main - finds and prints the sum of the even-valued terms
+* in Fibonacci sequence whose values do not exceed 4,000,000
+* Return: Always 0
+*/
 int main(void)
 {
 long int a = 1, b = 2, next, sum = 0;
-
 while (b <= 4000000)
 {
 if (b % 2 == 0)
 sum += b;
-
 next = a + b;
 a = b;
 b = next;
 }
-
 printf("%ld\n", sum);
 return (0);
 }
