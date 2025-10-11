688c05b815892b833a14d1920d3475cf7bbe4796
root <root@ip-10-42-95-115.ec2.internal>
2025-10-11 15:16:22 +0000

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
-* main - prints the first 98 Fibonacci numbers starting with 1 and 2
+* main - prints the first 98 Fibonacci numbers
 * Return: Always 0
 */
 int main(void)
