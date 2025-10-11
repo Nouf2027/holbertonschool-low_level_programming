17610617b708f0912b703d865df00815f7d40c47
root <root@ip-10-42-113-11.ec2.internal>
2025-10-11 07:56:10 +0000

    fix error

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
- int i, j;
+int i, j;
 
- for (i = 0; i < 100; i++)
- {
- for (j = i + 1; j < 100; j++)
- {
- putchar((i / 10) + '0');
- putchar((i % 10) + '0');
- putchar(' ');
- putchar((j / 10) + '0');
- putchar((j % 10) + '0');
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 
 if (!(i == 98 && j == 99))
 {
- putchar(',');
- putchar(' ');
+putchar(',');
+putchar(' ');
 }
 }
 }
