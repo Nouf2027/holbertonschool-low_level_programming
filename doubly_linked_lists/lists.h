e6e7a6e97e88bbfd52c68cbc2d4ea4e0b7c4e857
root <root@ip-10-42-161-50.ec2.internal>
2025-10-12 12:39:06 +0000

    Update lists.h: added prototype for sum_dlistint and other missing functions

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -5,25 +5,42 @@
 #include <stdlib.h>
 
 /**
- * struct dlistint_s - doubly linked list node
+ * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-int n;
-struct dlistint_s *prev;
-struct dlistint_s *next;
+ int n;
+ struct dlistint_s *prev;
+ struct dlistint_s *next;
 } dlistint_t;
 
-/* Function prototypes */
+/* === Function Prototypes for Tasks === */
+
+/* 0-print_dlistint.c */
 size_t print_dlistint(const dlistint_t *h);
+
+/* 1-dlistint_len.c */
 size_t dlistint_len(const dlistint_t *h);
+
+/* 2-add_dnodeint.c */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+/* 3-add_dnodeint_end.c */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+/* 4-free_dlistint.c */
 void free_dlistint(dlistint_t *head);
+
+/* 5-get_dnodeint_at_index.c */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* 6-sum_dlistint.c */
+int sum_dlistint(dlistint_t *head);
+
 #endif /* LISTS_H */
