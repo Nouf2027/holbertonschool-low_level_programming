e8af9dfc4b2558dca481a5cafa93d7fa6efadce0
root <root@ip-10-42-161-50.ec2.internal>
2025-10-12 11:02:41 +0000

    fix error 1

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return: nothing
  */
-void __ attribute__  ((constructor)) print_before_main(void)
+void   __attribute__  ((constructor)) print_before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
