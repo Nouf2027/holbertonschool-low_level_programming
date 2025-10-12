5de87bd872bdb9a2ec73f36b13c15c167e446c66
root <root@ip-10-42-161-50.ec2.internal>
2025-10-12 10:49:56 +0000

    Fix the errors

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,28 +1,20 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "lists.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
  *
- * Description: singly linked list node structure
+ * Return: nothing
  */
-typedef struct list_s
+void free_list(list_t *head)
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
+list_t *temp;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-
-#endif /* LISTS_H */
+while (head != NULL)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
