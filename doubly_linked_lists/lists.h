84304c38c2baad33f4993b07bc8246c634ab7d3d
root <root@ip-10-42-202-40.ec2.internal>
2025-10-13 09:52:15 +0000

    Add prototype for insert_dnodeint_at_index

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -42,5 +42,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 /* 6-sum_dlistint.c */
 int sum_dlistint(dlistint_t *head);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 #endif /* LISTS_H */
