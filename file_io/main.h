2e44740ee8e464cdfe69f58341ad9d675149021c
root <root@ip-10-42-224-211.ec2.internal>
2025-10-16 15:45:53 +0000

    Add 1-create_file.c

--- file_io/main.h
+++ file_io/main.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
-
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
