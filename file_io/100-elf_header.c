5855a102cfd5768b90996823d2291ca7834df436
root <root@ip-10-42-154-167.ec2.internal>
2025-10-17 07:25:59 +0000

    Fix: converted all ULL to UL for ISO C90 compliance

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -7,8 +7,17 @@
 #include <elf.h>
 #define DIE(msg, code) do { dprintf(STDERR_FILENO, "%s\n", (msg)); exit(code); } while (0)
 static unsigned short bswap16(unsigned short x){return (x>>8)|(x<<8);}
-static unsigned int bswap32(unsigned int x){return((x&0x000000FFU)<<24)|((x&0x0000FF00U)<<8)|((x&0x00FF0000U)>>8)|((x&0xFF000000U)>>24);}
-static unsigned long bswap64(unsigned long x){return((x&0x00000000000000FFULL)<<56)|((x&0x000000000000FF00ULL)<<40)|((x&0x0000000000FF0000ULL)<<24)|((x&0x00000000FF000000ULL)<<8)|((x&0x000000FF00000000ULL)>>8)|((x&0x0000FF0000000000ULL)>>24)|((x&0x00FF000000000000ULL)>>40)|((x&0xFF00000000000000ULL)>>56);}
+static unsigned long bswap64(unsigned long x)
+{
+return ((x & 0x00000000000000FFUL) << 56) |
+((x & 0x000000000000FF00UL) << 40) |
+((x & 0x0000000000FF0000UL) << 24) |
+((x & 0x00000000FF000000UL) << 8)  |
+((x & 0x000000FF00000000UL) >> 8)
+((x & 0x0000FF0000000000UL) >> 24) |
+((x & 0x00FF000000000000UL) >> 40) |
+((x & 0xFF00000000000000UL) >> 56);
+}
 static void print_magic(unsigned char *e_ident){size_t i;printf("ELF Header:\n");printf("  Magic:   ");for(i=0;i<EI_NIDENT;i++)printf("%02x%s",e_ident[i],i+1==EI_NIDENT?"":" ");printf("\n");}
 static const char *class_str(unsigned char c){if(c==ELFCLASS32)return"ELF32";if(c==ELFCLASS64)return"ELF64";return"none";}
 static const char *data_str(unsigned char d){if(d==ELFDATA2LSB)return"2's complement, little endian";if(d==ELFDATA2MSB)return"2's complement, big endian";return"none";}
