d09af86b726d93dc4bd8536184efeaa1c4ebdc9d
root <root@ip-10-42-117-94.ec2.internal>
2025-10-19 03:31:15 +0000

    Fix: add Betty descriptions for struct members

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -9,7 +9,7 @@
  * struct hash_node_s - Node of a hash table
  * key: The key string
  * value: The value associated with the key
- * next: Pointer to the next node in the list
+ * next: Points to the next node in the list
  */
 typedef struct hash_node_s
 {
@@ -21,7 +21,7 @@ struct hash_node_s *next;
 /**
  * struct hash_table_s - Hash table data structure
  * size: The size of the array
- * array: The array of pointers to hash_node_t
+ * array: The array of pointers to nodes
  */
 typedef struct hash_table_s
 {
@@ -38,4 +38,3 @@ hash_node_t **array;
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif /* HASH_TABLES_H */
-
