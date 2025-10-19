b26740e1caa7a523b8206924353f17018edceb9a
root <root@ip-10-42-117-94.ec2.internal>
2025-10-19 05:02:17 +0000

    A

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,19 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create a new hash table.
- * @size: Number of slots to allocate for the table array.
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: Pointer to the new table, or NULL on allocation failure.
+ * Return: Pointer to the newly created hash table,
+ * or NULL if something went wrong.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
- hash_table_t *table;
- unsigned long int i;
+hash_table_t *table;
+unsigned long int i;
 
-  table = malloc(sizeof(hash_table_t));
-  if (table == NULL)
-  return (NULL);
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
 
 table->size = size;
 table->array = malloc(sizeof(hash_node_t *) * size);
