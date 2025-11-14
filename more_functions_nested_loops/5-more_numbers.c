c08ec9eb435e9e43b2f81d1fcab2a26a0395c380
root <root@ip-10-42-245-187.ec2.internal>
2025-11-14 18:24:25 +0000

    Add file

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14, ten times
+ */
+void more_numbers(void)
+{
+int line,n;
+for(line=0;line<10;line++)
+{
+for(n=0;n<=14;n++)
+{
+if(n>=10)_putchar('1');
+_putchar((n%10)+'0');
+}
+_putchar('\n');
+}
+}
