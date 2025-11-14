d6f8ce2890aeac30f913664b261d00265311d203
root <root@ip-10-42-245-187.ec2.internal>
2025-11-14 19:03:14 +0000

    1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * more_numbers - prints numbers from 0 to 14 ten times
- *
- * Return: void
- */
+* more_numbers - prints numbers from 0 to 14, ten times
+*/
 void more_numbers(void)
 {
-int line,n;
-for(line=0;line<10;line++)
+int i;
+int x;
+for (i = 0; i < 10; i++)
 {
-for(n=0;n<=14;n++)
+for (x = 0; x <= 14; x++)
 {
-if(n>=10)_putchar('1');
-_putchar((n%10)+'0');
+if (x > 9)
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
 }
 _putchar('\n');
 }
