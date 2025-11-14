fdcb71a53273c7e4b79b263bc7c9ab91a4b46252
root <root@ip-10-42-245-187.ec2.internal>
2025-11-14 20:04:10 +0000

    Fix: added prototype for _putchar and include main.h

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,7 @@
+#include "main.h"
+
 /**
- * print_square - prints a square with '#'
+ * print_square - prints a square of '#'
  * @size: size of the square
  *
  * Return: nothing
@@ -7,11 +9,13 @@
 void print_square(int size)
 {
 int i, j;
+
 if (size <= 0)
 {
 _putchar('\n');
 return;
 }
+
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
