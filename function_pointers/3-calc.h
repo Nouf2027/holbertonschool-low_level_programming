339d8bbcb3c587587d1fbc9c36052a1a9241cd19
Nouf Almutairi <noufn8988@gmail.com>
2026-06-12 00:19:47 +0300

    Create 3-calc.h

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,23 @@
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
+
+/**
+ * struct op - Struct for operator and its function
+ * @op: The operator as a string
+ * @f: The function corresponding to the operator
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* _3_CALC_H_ */
