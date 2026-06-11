0e8e7d458212eec7483f4d68a7a50e01c96163a5
Nouf Almutairi <noufn8988@gmail.com>
2026-06-12 00:42:36 +0300

    Update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,11 +3,13 @@
 
 #include <stddef.h>
 
-/**
- * print_name - prints a name using a function pointer
- * @name: the name to be printed
- * @f: pointer to a function that prints the name
- */
+/* Task 0 */
 void print_name(char *name, void (*f)(char *));
+
+/* Task 1 */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Task 2 */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
